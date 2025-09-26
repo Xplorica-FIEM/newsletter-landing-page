ac6edcf81f69d4d815a476c9fb7034a442414ee2
Saptarshi Chakraborty <saptarshichakraborty04@outlook.com>
2025-09-26 05:51:17 +0530

    Enhance SEO and social sharing features with updated meta tags and Open Graph data in index.js; add welcome email template for new subscribers

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,28 @@ const Home = () => {
     <>
       <Head>
         <title>Newsletter | Xplorica</title>
-        <meta name="description" content="Stay updated with the latest news and insights from Xplorica." />
+        <meta name="description" content="Stay updated with the latest news and insights from Xplorica. Your go-to source for content writing, tech, and innovation." />
+        <meta name="keywords" content="Xplorica, newsletter, travel, tech, innovation, updates, insights" />
+        <meta name="author" content="Xplorica" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="canonical" href="https://newsletter.xplorica.in/" />
         <link rel="icon" href="/favicon.png" />
+        <meta name="robots" content="index, follow" />
+
+        {/* Open Graph Meta Data */}
+        <meta property="og:title" content="Newsletter | Xplorica" />
+        <meta property="og:description" content="Stay updated with the latest news and insights from Xplorica." />
+        <meta property="og:image" content="https://newsletter.xplorica.in/xplorica-logo-with-text.png" />
+        <meta property="og:url" content="https://newsletter.xplorica.in" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Newsletter |Xplorica" />
+
+        {/* Twitter Card Meta Data */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Newsletter | Xplorica" />
+        <meta name="twitter:description" content="Stay updated with the latest news and insights from Xplorica." />
+        <meta name="twitter:image" content="https://newsletter.xplorica.in/xplorica-logo-with-text.png" />
+        
 
         {/* Prefetch the Xplorica logo */}
         <link rel="preload" href="/xplorica-logo.png" as="image" />
